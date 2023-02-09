359d721a0363bead9eefb0a87b9ce6dfbcbdd2d8
MehulBuha <mehulbuha.megaminds@gmail.com>
2023-02-09 22:15:02 +0530

    add to new demo

---
+++ PracticalDemo/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracticalDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(tbl_user2 objuser)
+        {
+            PracticalDemoEntities2 db = new PracticalDemoEntities2();
+           var user= db.tbl_user2.Where(x => x.UserName == objuser.UserName && x.Email == objuser.Email).Count();
+            if(user>0)
+            {
+                return RedirectToAction("ListUsers");
+            }
+            else
+            {
+                return View();
+            }
+           
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult Register(tbl_user2 user)
+        {
+
+            PracticalDemoEntities2 db = new PracticalDemoEntities2();
+            db.tbl_user2.Add(user);
+            db.SaveChanges();
+            return RedirectToAction("ListUsers");
+        }
+
+        public ActionResult ListUsers()
+        {
+
+
+            PracticalDemoEntities2 db = new PracticalDemoEntities2();
+           
+
+            var users = db.tbl_user2.ToList();
+
+            
+            return View(users);
+        }
+
+        public ActionResult UserProfile(int id)
+        {
+            PracticalDemoEntities2 db = new PracticalDemoEntities2();
+
+            var user = db.tbl_user2.Find(id);
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult UserProfile(tbl_user2 objuser)
+        {
+            PracticalDemoEntities2 db = new PracticalDemoEntities2();
+            db.Entry(objuser).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+
+            return View();
+        }
+    }
+}
---
+++ PracticalDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PracticalDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PracticalDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("be5b402e-417f-4953-b413-9034db1c71bb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
